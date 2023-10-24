772e8a0282a25d01305863c4a5fac26315d5f809
Eric <eric.ruiz@enti.cat>
2023-10-24 10:27:08 +0200

    past camera test

--- Assets/Project/Modules/PlayerController/Scripts/Camera/OrbitingCamera.cs
+++ Assets/Project/Modules/PlayerController/Scripts/Camera/OrbitingCamera.cs
@@ -13,7 +13,7 @@ public class OrbitingCamera : MonoBehaviour
     private Vector3 _focusPoint;
     [HideInInspector] public Vector3 focusPointOffset;
 
-    [SerializeField, Range(1.0f, 100.0f)] private float _distance;
+    [SerializeField, Range(1.0f, 300.0f)] public float _distance;
     [SerializeField, Range(0.0f, 10.0f)] private float _noFocusRadius;
 
     [SerializeField, Range(0.0f, 1.0f)] private float _stillFocusRecentering = 0.5f;
